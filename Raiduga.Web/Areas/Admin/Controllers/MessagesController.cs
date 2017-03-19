94940a14ba9345123f4acd79c582525e96da49fd
Anton Valintsev <valintsev.anton@gmail.com>
2017-03-19 14:57:14 +0200

    FIxing null reference on messages

--- Raiduga.Web/Areas/Admin/Controllers/MessagesController.cs
+++ Raiduga.Web/Areas/Admin/Controllers/MessagesController.cs
@@ -1,44 +1,45 @@
 ﻿namespace Raiduga.Web.Areas.Admin.Controllers
 {
-	using Autofac;
-	using Raiduga.Web.Areas.Admin.Controllers.Base;
-	using Raiduga.Web.Models.Common;
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Web.Mvc;
+    using Autofac;
+    using Raiduga.Web.Areas.Admin.Controllers.Base;
+    using Raiduga.Web.Models.Common;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
 
-	public class MessagesController : BaseAdminController
-	{
-		public MessagesController(IComponentContext componentContext)
-		{
-		}
+    public class MessagesController : BaseAdminController
+    {
+        public MessagesController(IComponentContext componentContext)
+            : base(componentContext)
+        {
+        }
 
-		// GET: Admin/Admin
-		public ActionResult Index()
-		{
-			var dbData = DbContext.ContactRequests.OrderByDescending(cr => cr.CreationDate);
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            var dbData = DbContext.ContactRequests.OrderByDescending(cr => cr.CreationDate);
 
-			var model = new List<ContactRequestViewModel>();
-			foreach (var dbItem in dbData)
-			{
-				model.Add(_modelTransformer.GetViewModel<ContactRequestViewModel>(dbItem));
-			}
+            var model = new List<ContactRequestViewModel>();
+            foreach (var dbItem in dbData)
+            {
+                model.Add(_modelTransformer.GetViewModel<ContactRequestViewModel>(dbItem));
+            }
 
-			return View(model);
-		}
+            return View(model);
+        }
 
-		[HttpPost]
-		public ActionResult MarkAsRead(FormCollection form)
-		{
-			var itemId = int.Parse(form["cr.Id"]);
+        [HttpPost]
+        public ActionResult MarkAsRead(FormCollection form)
+        {
+            var itemId = int.Parse(form["cr.Id"]);
 
-			var dbItem = DbContext.ContactRequests.Find(itemId);
-			dbItem.UpdationDate = DateTime.Now;
+            var dbItem = DbContext.ContactRequests.Find(itemId);
+            dbItem.UpdationDate = DateTime.Now;
 
-			DbContext.SaveChanges();
+            DbContext.SaveChanges();
 
-			return RedirectToAction("Index");
-		}
-	}
+            return RedirectToAction("Index");
+        }
+    }
 }
